754561d6834d7104e0493fd9fa0dd0de07e24511
irenejea <irene.jea@utexas.edu>
2014-12-08 16:18:06 -0600

    Create profile.php
    "Your Profile" in navigation and the user name in the top right links to this page

---
+++ profile.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Profile</title>
+	<link rel="stylesheet" href="./outline.css" type="text/css" name="style1">
+</head>
+
+<div id="container">
+
+	<a href="./home.php"><img src="./logo.png" id="logo"></a>
+
+<div id="title">
+	<h1>Profile</h1>
+</div>
+
+
+<div id="info">
+	<center><div id="info_title">Brain Surge Site Navigation</div></center>
+	<h4><i>Personality Quizzes</i></h4>
+	<ul><li><a href="./mb.php">MBTI</a></li>
+	    <li><a href="./big5.php">The Big Five</a></li>
+	    <li><a href="./typeAB.php">Type A/B</a></li>
+	</ul>
+	<h4 style="color: gray"><i>Your Profile</i></h4>
+	<h4><i>More Information</h4></i>
+	<dl><dt>Personality Theory</dt>
+		<dd><a href="https://en.wikipedia.org/wiki/Personality_psychology">Personality Psychology</a></dd>
+		<dd><a href="http://drdianehamilton.wordpress.com/2011/12/18/top-18-personality-theorists-including-freud-and-more/">Famous Personality Psychologists</a></dd>
+	<br>
+	    <dt>Social Psychology</dt>
+		<dd><a href="https://en.wikipedia.org/wiki/Social_psychology">Social Psychology</a></dd>
+		<dd><a href="http://www.ippanetwork.org/">Positive Psychology</a></dd>
+	<br>
+	    <dt>Neuropsychology</dt>
+		<dd><a href="https://en.wikipedia.org/wiki/Neuropsychology">Neuropsychology</a></dd>
+		<dd><a href="http://www.neuroaustin.com/">Local Neuropsychologists</a></dd>
+	<br>
+	    <dt>Other Fields of Psychology</dt>
+		<dd><a href="https://en.wikipedia.org/wiki/Cognitive_psychology">Cognitive Psychology</a></dd>
+		<dd><a href="https://en.wikipedia.org/wiki/Developmental_psychology">Developmental Psychology</a></dd>
+		<dd><a href="https://en.wikipedia.org/wiki/Clinical_psychology">Clinical Psychology</a></dd>
+		<dd><a href="https://en.wikipedia.org/wiki/Abnormal_psychology">Abnormal Psychology</a></dd>
+	</dl>
+	
+	<h4><i>Contact Brain Surge</i></h4>
+		<p><a href="./info.php">About Brain Surge and the developers</a></p>
+</div>
+
+</div>
+</html>
