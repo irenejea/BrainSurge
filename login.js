9331473a4f22f9d1b4855370748fdfc1e84de440
irenejea <irene.jea@utexas.edu>
2014-12-08 16:19:17 -0600

    Create login.js
    moved the javascript for form validation here since login.php was getting too long

---
+++ login.js
@@ -0,0 +1,105 @@
+
+$(document).ready(function(){
+	$("#fn").on("input", function(){
+		var input=$(this);
+		var first=input.val();
+		if(first){
+			input.removeClass("invalid").addClass("valid");
+		}
+		else{
+			input.removeClass("valid").addClass("invalid");
+		}
+	});
+	$("#ln").on("input", function(){
+		var input=$(this);
+		var last=input.val();
+		if(last){
+			input.removeClass("invalid").addClass("valid");
+		}
+		else{
+			input.removeClass("valid").addClass("invalid");
+		}
+	});
+	$("#user").on("input", function(){
+		var input=$(this);
+		var id=input.val();
+		if(id){
+			input.removeClass("invalid").addClass("valid");
+		}
+		else{
+			input.removeClass("valid").addClass("invalid");
+		}
+	});
+	$("#pass").on("input", function(){
+		var input=$(this);
+		var pw=input.val();
+		if(pw){
+			input.removeClass("invalid").addClass("valid");
+		}
+		else{
+			input.removeClass("valid").addClass("invalid");
+		}
+	});
+	$("#id").on("input", function(){
+		var input=$(this);
+		var id=input.val();
+		if(id){
+			input.removeClass("invalid").addClass("valid");
+		}
+		else{
+			input.removeClass("valid").addClass("invalid");
+		}
+	});
+	$("#pw").on("input", function(){
+		var input=$(this);
+		var pw=input.val();
+		if(pw){
+			input.removeClass("invalid").addClass("valid");
+		}
+		else{
+			input.removeClass("valid").addClass("invalid");
+		}
+	});
+	
+	$("#submitR").click(function(event){
+		var go=true;
+		var form=$("#formR").serializeArray();
+		for(var input in form){
+			var val=$("#"+form[input]['name']);
+			var valid=val.hasClass("valid");
+			var elmnt=$("span", val.parent());
+			if(!valid){
+				elmnt.removeClass("error").addClass("error_show");
+				go=false;
+			}
+			else{
+				elmnt.removeClass("error_show").addClass("error");
+			}
+		}
+		if(!go){
+			event.preventDefault();
+		}
+	});
+	$("#submitL").click(function(event){
+		var go=true;
+		var form=$("#formL").serializeArray();
+		for(var input in form){
+		var user=$("#"+form[0]['name']);
+		var validUser=user.hasClass("valid");
+		var pass=$("#"+form[1]['name']);
+		var validPass=pass.hasClass("valid");
+		var error = $("span", pass.parent());
+		if(!validUser || !validPass){
+			error.removeClass("error").addClass("error_show");
+				go=false;
+			}
+			else{
+				error.removeClass("error_show").addClass("error");
+			}
+		}
+		if(!go){
+			event.preventDefault();
+		}
+	});
+})
+
